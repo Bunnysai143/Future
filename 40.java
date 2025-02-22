0c65766af229e1fc360cabcef059e629c26b0c16
saikiranmain1708@gmail.com <saikiranmain1708@gmail.com>
2025-02-22 22:23:21 +0530

    hel

--- 40.java
+++ 40.java
@@ -24,3 +24,59 @@ class Main {
         System.out.print(ans);
     }
 }
+
+
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int nums[]={2,2,2,1,2,2,1,2,2,2};
+        int k=2;
+        int l=0; 
+        int len=nums.length;
+        int temp=0;
+        int ans=0;
+        for(int r=0;r<len;r++){
+            if(nums[r]%2!=0){
+                temp+=1;
+            }
+            System.out.println(temp);
+            while(temp>=k){
+                if(nums[l]%2!=0){
+                    temp-=nums[l];
+                }
+                 l++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
+//1248
+class Solution {
+    public int atMostK(int[] nums,int k){
+        int len=nums.length;
+        int ans=0;
+        int l=0;
+        int temp=0;
+        for(int r=0;r<len;r++){
+            if(nums[r]%2==1){
+                temp+=1;
+            }
+            while(temp>k){
+                if(nums[l]%2==1){
+                    temp-=1;
+                }
+                l++;
+            }
+            ans+=r-l+1; 
+        }
+        return ans;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+       int ans=atMostK(nums,k)-atMostK(nums,k-1);
+       return ans;
+    }
+}
