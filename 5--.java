9e600399a7a675c891908b3d6a3fd941a3123e78
saikiranmain1708@gmail.com <saikiranmain1708@gmail.com>
2025-03-11 11:03:28 +0530

    binarySearch

--- 5--.java
+++ 5--.java
@@ -39,3 +39,42 @@ class Main {
         
     }
 }
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static boolean check(int[] position,int m,int speed){
+        int demo=position[0];
+        m-=1;
+        for(int i=1;i<position.length;i++){
+            if(position[i]-demo>=speed){
+                m-=1;
+                demo=position[i];
+            }
+        }
+        System.out.println(m);
+        if((m)>0){
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+        int[] position={1,2,3,4,7};
+        Arrays.sort(position);
+        int m= 3;
+        int l=1;
+        int r=1000000000;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(!check(position,m,mid)){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        System.out.println(r);
+    }
+}
